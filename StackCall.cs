c12f9e464e3da31d34a7cf2ae0c1568f8ad2e551
Jesse Davis <secabstraction@users.noreply.github.com>
2015-05-06 16:32:37 -0500

    Update StackCall.cs

--- StackCall.cs
+++ StackCall.cs
@@ -1,30 +1,30 @@
 using System;
 using PowerWalker.Natives;
 using System.Collections.Generic;
-
 namespace PowerWalker
 {
     public class StackCall
     {
-        public ulong Offset { get; private set; }
-        public ulong ReturnAddress { get; private set; }
+        public int ThreadId { get; private set; }
+        public ulong AddrPC { get; private set; }
+        public ulong AddrReturn { get; private set; }
         public string Symbol { get; private set; }
-        public string SymbolFile { get; private set; }
-
-        public StackCall(IntPtr hProcess, ulong AddrPC, ulong AddrReturn)
+        public string MappedFile { get; private set; }
+        
+        public StackCall(IntPtr hProcess, ulong AddrPC, ulong AddrReturn, int ThreadId)
         {
+            this.ThreadId = ThreadId;
+            this.AddrPC = AddrPC;
+            this.AddrReturn = AddrReturn;
+
             System.Text.StringBuilder ReturnedString = new System.Text.StringBuilder(256);
 
             IntPtr PcOffset = (IntPtr)Functions.UlongToLong(AddrPC);
-
             Psapi.GetMappedFileNameW(hProcess, PcOffset, ReturnedString, (uint)ReturnedString.Capacity);
-            SymbolFile = ReturnedString.ToString();
+            MappedFile = ReturnedString.ToString();
 
             IMAGEHLP_SYMBOL64 PcSymbol = Functions.GetSymbolFromAddress(hProcess, AddrPC);
             Symbol = new string(PcSymbol.Name);
-
-            Offset = AddrPC;
-            ReturnAddress = AddrReturn;
         }
     }
 }
