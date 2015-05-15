d38dc52523df1e4e5b6828a9efc974ad8b0b3dab
Jesse Davis <secabstraction@users.noreply.github.com>
2015-05-15 15:28:03 -0500

    Create Get-SymbolFromAddress.ps1

---
+++ PSReflected/Get-SymbolFromAddress.ps1
@@ -0,0 +1,18 @@
+function Get-SymbolFromAddress ($hProcess, $Address) {
+    
+    #Initialize params for SymGetSymFromAddr64
+    $Symbol = New-Object IMAGEHLP_SYMBOLW64
+    $Symbol.SizeOfStruct = [IMAGEHLP_SYMBOLW64]::GetSize()
+    $Symbol.MaxNameLength = 32
+
+    $lpSymbol = [System.Runtime.InteropServices.Marshal]::AllocHGlobal([System.Runtime.InteropServices.Marshal]::SizeOf($Symbol))
+    [System.Runtime.InteropServices.Marshal]::StructureToPtr($Symbol, $lpSymbol, $false)
+    [UInt64]$Offset = 0
+
+    [Win32.dbgHelp]::SymGetSymFromAddr64($hProcess, $Address, [ref]$Offset, $lpSymbol)
+            
+    $Symbol = [IMAGEHLP_SYMBOL64][System.Runtime.InteropServices.Marshal]::PtrToStructure($lpSymbol, [IMAGEHLP_SYMBOL64])
+    [System.Runtime.InteropServices.Marshal]::FreeHGlobal($lpSymbol)
+
+    return $Symbol
+}
